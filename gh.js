3685d4117329072b358ce0670ddf04cd5b734bca
triple93r <56282248+triple93r@users.noreply.github.com>
2023-10-24 09:53:31 +0530

    init3

---
+++ gh.js
@@ -0,0 +1,9 @@
+var firstName = "DDD"; // type string
+console.log(firstName);
+var u = "games";
+console.log(u);
+var x = "hhh";
+console.log(x);
+var jj = 9;
+//jj = 99;
+console.log(jj);
---
+++ gh.ts
@@ -0,0 +1,9 @@
+let firstName = "DDD"; // type string
+console.log(firstName);
+let u  = "games";
+console.log(u);
+var x = "hhh"
+console.log(x);
+const jj = 9;
+//jj = 99;
+console.log(jj);
